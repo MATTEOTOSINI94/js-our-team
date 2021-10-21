9d32d8fabb8e318de4dff16f9ce8bbe4d277ce8c
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-21 15:14:15 +0200

    mod

---
+++ js/master.js
@@ -0,0 +1,54 @@
+const cardContainer = document.querySelector(".team-container")
+console.log(cardContainer)
+
+const card =[
+{
+    nome:"Angela Karol",
+    ruolo:"Chief Editor",
+    immagine:"img/angela-caroll-chief-editor.jpg",
+},
+{
+    nome:"Walter Gordon",
+    ruolo:"Official Manger",
+    immagine:"img/angela-lopez-social-media-manager.jpg",
+},
+{
+    nome:"Angela Lopez",
+    ruolo:"Social Media Manager",
+    immagine:"img/scott-estrada-developer.jpg",
+},
+{
+    nome:"Scott Estrada",
+    ruolo:"Developer",
+    immagine:"img/walter-gordon-office-manager.jpg",
+},
+{
+    nome:"Barbara Ramos",
+    ruolo:"Graphic Designer",
+    immagine:"img/wayne-barnett-founder-ceo.jpg",
+}
+
+]
+
+for (let x = 0; x < card.length; x++) {
+    const indiceCard = card[x];
+    console.log(indiceCard)
+
+    const cardWork =  `<div class="team-card">
+    <div class="card-image">
+        <img
+            src="${indiceCard.immagine}"
+            alt="Wayne Barnett"
+        />
+        </div>
+        <div class="card-text">
+        <h3>${indiceCard.nome}</h3>
+        <p>${indiceCard.ruolo}</p>
+        </div>
+     </div>
+    </div>`
+
+    cardContainer.innerHTML += cardWork
+    
+}
+

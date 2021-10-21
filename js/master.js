f8b2a767b24b38362a6ed392007555e2f49424a7
MATTEOTOSINI94 <matteotosini300@icloud.com>
2021-10-21 16:40:56 +0200

    mod

--- js/master.js
+++ js/master.js
@@ -1,40 +1,43 @@
+const btnForm = document.getElementById("addMemberButton")
 const cardContainer = document.querySelector(".team-container")
 console.log(cardContainer)
 
-const card =[
-{
-    nome:"Angela Karol",
-    ruolo:"Chief Editor",
-    immagine:"img/angela-caroll-chief-editor.jpg",
-},
-{
-    nome:"Walter Gordon",
-    ruolo:"Official Manger",
-    immagine:"img/angela-lopez-social-media-manager.jpg",
-},
-{
-    nome:"Angela Lopez",
-    ruolo:"Social Media Manager",
-    immagine:"img/scott-estrada-developer.jpg",
-},
-{
-    nome:"Scott Estrada",
-    ruolo:"Developer",
-    immagine:"img/walter-gordon-office-manager.jpg",
-},
-{
-    nome:"Barbara Ramos",
-    ruolo:"Graphic Designer",
-    immagine:"img/wayne-barnett-founder-ceo.jpg",
-}
+const card = [
+    {
+        nome: "Angela Karol",
+        ruolo: "Chief Editor",
+        immagine: "img/angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Official Manger",
+        immagine: "img/angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social Media Manager",
+        immagine: "img/scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        immagine: "img/walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        immagine: "img/wayne-barnett-founder-ceo.jpg",
+    }
 
 ]
+
+
 
 for (let x = 0; x < card.length; x++) {
     const indiceCard = card[x];
     console.log(indiceCard)
 
-    const cardWork =  `<div class="team-card">
+    const cardWork = `<div class="team-card">
     <div class="card-image">
         <img
             src="${indiceCard.immagine}"
@@ -49,6 +52,52 @@ for (let x = 0; x < card.length; x++) {
     </div>`
 
     cardContainer.innerHTML += cardWork
-    
 }
 
+
+
+
+
+btnForm.addEventListener("click", function () {
+    //    lettura dati inseriti da input (nome/ruolo/nome dell'immagine)
+    //    creazione oggetto con i dati presi da input
+    //    creare la card nella mia interfaccia
+    //   inserire il nuovo oggetto nel mio array
+
+
+    const nameUser = document.getElementById("name").value
+    const ruoloUser = document.getElementById("role").value
+    const imgUser = document.getElementById("image")
+    console.log(nameUser)
+    const cardUser = {
+
+        name: nameUser,
+        ruolo: ruoloUser,
+        pic: imgUser,
+    }
+    console.log(cardUser)
+
+     const cardboh =  `<div class="team-card">
+     <div class="card-image">
+         <img
+             src="img/wayne-barnett-founder-ceo.jpg"
+             alt="Wayne Barnett"
+         />
+         </div>
+         <div class="card-text">
+         <h3>${cardUser.name}</h3>
+         <p>${cardUser.ruolo}</p>
+         </div>
+      </div>
+     </div>`
+
+     cardContainer.innerHTML += cardboh
+
+})
+
+
+
+
+function genraImg() {
+
+}
